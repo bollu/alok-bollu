212c745048c4426cba6cbe812c9ae4ba6e900701
Siddharth Bhat <siddu.druid@gmail.com>
2019-04-16 19:20:16 +0530

    change batch file to store model outputs in unique files for unique runs

--- word2blank/word2vec/sbatch-train-sg.sh
+++ word2blank/word2vec/sbatch-train-sg.sh
@@ -5,5 +5,10 @@
 #SBATCH --job-name=w2m
 #SBATCH --mail-type=END
 #SBATCH -o ./slurm/%j
+DATE=`date '+%Y-%m-%d--%H:%M:%S'`
+FOLDERNAME=$(git rev-parse --short HEAD)---$SLURM_ARRAY_JOB_ID---$DATE
+mkdir -p models/$FOLDERNAME
+mkdir -p slurm/
+
 make word2vec
-time ./word2vec -train text8 -output vectors-sg.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
+time ./word2vec -train text8 -output models/$FOLDERNAME/vectors-sg.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15 

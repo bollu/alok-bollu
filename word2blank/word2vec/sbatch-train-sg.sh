4960db85506018ac46ab6945ef548bc32bdfedef
Siddharth Bhat <siddu.druid@gmail.com>
2019-04-17 17:30:30 +0530

    make file name git log name

--- word2blank/word2vec/sbatch-train-sg.sh
+++ word2blank/word2vec/sbatch-train-sg.sh
@@ -6,9 +6,10 @@
 #SBATCH --mail-type=END
 #SBATCH -o ./slurm/%j
 DATE=`date '+%Y-%m-%d--%H:%M:%S'`
-FOLDERNAME=$(git rev-parse --short HEAD)---$SLURM_ARRAY_JOB_ID---$DATE
+GITNAME=$(git rev-parse --short HEAD)
+FOLDERNAME=$GITNAME---$SLURM_ARRAY_JOB_ID---$DATE
 mkdir -p models/$FOLDERNAME
 mkdir -p slurm/
 
 make word2vec
-time ./word2vec -train text8 -output models/$FOLDERNAME/vectors-sg.bin -cbow 0 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 30 -binary 1 -iter 15 
+time ./word2vec -train text8 -output models/$FOLDERNAME/$GITNAME.bin -cbow 0 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 30 -binary 1 -iter 15 

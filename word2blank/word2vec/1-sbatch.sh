d8532bcf5779b347b8a53336499152432ed9edb8
Siddharth Bhat <siddu.druid@gmail.com>
2019-04-17 22:37:04 +0530

    rename w2b to w2v

--- word2blank/word2vec/1-sbatch.sh
+++ word2blank/word2vec/1-sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH -p long
 #SBATCH --time=72:00:00
 #SBATCH --nodes=1
-#SBATCH --job-name=w2m
+#SBATCH --job-name=w2v
 #SBATCH --mail-type=END
 #SBATCH -o ./slurm/%j
 DATE=`date '+%Y-%m-%d--%H:%M:%S'`

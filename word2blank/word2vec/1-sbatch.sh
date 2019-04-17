0a03bc5b6653902cfbdf98aa6ac6d65513f5e5e1
Siddharth Bhat <siddu.druid@gmail.com>
2019-04-18 00:18:31 +0530

    teach sbatch to symlink file

--- word2blank/word2vec/1-sbatch.sh
+++ word2blank/word2vec/1-sbatch.sh
@@ -5,14 +5,22 @@
 #SBATCH --job-name=w2v
 #SBATCH --mail-type=END
 #SBATCH -o ./slurm/%j
+
+### SET NAME ###
+NAME=XXXXXXX
+########
+########
+
+
 DATE=`date '+%Y-%m-%d--%H:%M:%S'`
 GITNAME=$(git rev-parse --short HEAD)
 FOLDERNAME=$GITNAME
-mkdir -p models/$FOLDERNAME
+mkdir -p models/
 mkdir -p slurm/
 
 make word2vec
 time ./word2vec -train text8 -output models/$GITNAME.bin -cbow 0 -size 10 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15 
 ./1-save-models.sh
-./1-eval.sh models/$GITNAME.bin
+$(cd models; ln -s $GITNAME.bin $NAME; cd ../)
+./1-eval.sh models/$NAME.bin
 ./1-save-models.sh

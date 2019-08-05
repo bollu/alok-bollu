183be310b1535374076660a0584046bb308f0792
Alok Debnath <alok.debnath@research.iiit.ac.in>
2019-08-06 01:19:24 +0530

    ablations for one random vector (syn0) tested

--- word2blank/word2vec/ablations/1-sbatch.sh
+++ word2blank/word2vec/ablations/1-sbatch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#SBATCH -A sub
 #SBATCH -p long
-#SBATCH --time=72:00:00
+#SBATCH --time=6:00:00
 #SBATCH --nodes=1
 #SBATCH --job-name=w2v
 #SBATCH --mail-type=END
@@ -21,8 +22,6 @@ NAME=ablations
 mkdir -p ablation_models/
 mkdir -p slurm/
 
-make word2vec word2vec_two_random_vectors                                        
-time ./word2vec -train text8 -output ./ablation_models/naive_text8.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 40 -binary 1 -    iter 15
-time ./word2vec_two_random_vectors -train text8 -output ./ablation_models/two_random_vectors_text8.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sam    ple 1e-4 -threads 40 -binary 1 -iter 15
-./compute-accuracy ./ablation_models/naive_text8.bin < ../questions-words.txt > ./ablation_models/naive_text8_accuracies.txt
-./compute-accuracy ./ablation_models/two_random_vectors_text8.bin < ../questions-words.txt > ./ablation_models/two_random_vectors_text8_accuracies.txt        
+make word2vec_one_random_vector                                        
+time ./word2vec_one_random_vector -train text8 -output ./ablation_models/one_random_vector_text8.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 40 -binary 1 -iter 15
+./compute-accuracy ./ablation_models/one_random_vector_text8.bin < ../questions-words.txt > ./ablation_models/one_random_vector_text8_accuracies.txt        
--- word2blank/word2vec/ablations/demo-word.sh
+++ word2blank/word2vec/ablations/demo-word.sh
@@ -4,6 +4,5 @@ if [ ! -e text8 ]; then
   gzip -d text8.gz -f
 fi
 
-time ./word2vec_two_random_vectors -train text1 -output ./ablation_models/two_random_vectors.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
-./distance ./ablation_models/two_random_vectors.bin
+time ./word2vec_two_random_vectors -train text1 -output ./ablation_models/two_random_vectors.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15 ./distance ./ablation_models/two_random_vectors.bin
 

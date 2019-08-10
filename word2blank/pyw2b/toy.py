97c2708f4156ff6626014c4d4bbcae916b76fb5a
Siddharth Bhat <siddu.druid@gmail.com>
2019-08-11 04:11:36 +0530

    code seems to do something

--- word2blank/pyw2b/toy.py
+++ word2blank/pyw2b/toy.py
@@ -23,17 +23,18 @@ import threading
 from numpy import random
 
 CORPUSPATH="text1"
-NUMTHREADS = 20
+NUMTHREADS = 1
 BATCHSIZE = 1000
 WINDOWSIZE = 4
-EPOCHS = 10
-EMBEDSIZE = 100
-STARTING_ALPHA = 0.01
-# DEVICE = torch.device(torch.cuda.device_count() - 1) if torch.cuda.is_available() else torch.device('cpu')
-DEVICE = torch.device("cpu")
+EPOCHS = 300
+EMBEDSIZE = 3
+STARTING_ALPHA = 1
+DEVICE = torch.device(torch.cuda.device_count() - 1) if torch.cuda.is_available() else torch.device('cpu')
+# DEVICE = torch.device("cpu")
 
 with open(CORPUSPATH, "r") as f:
-    corpus = np.array(f.read().split())
+    corpus = np.array(f.read().split())[:1000]
+print("corpus:\n", corpus)
 print("corpus length: ", len(corpus))
 
 print("building vocab...")
@@ -56,7 +57,7 @@ for i in range(len(corpus)):
     ixedcorpus[i] = vocabIxs[corpus[i]]
 
 print("ixed corpus: ", ixedcorpus)
-part = len(corpus) // 10
+part = max(len(corpus) // 10, 1)
 for i in range(len(corpus)):
     iix = ixedcorpus[i]
     if i % part == 0: print("*")
@@ -115,12 +116,13 @@ def perthread(tid):
     end = min((tid + 1) * PER_THREAD_SIZE, len(corpus) - 1)
 
     cur_corpus = corpus[start:end]
-    per_epoch = len(cur_corpus) // BATCHSIZE
+    per_epoch = max(1, len(cur_corpus) // BATCHSIZE)
     print("%d | per epoch: %d" % (tid, per_epoch))
     total = EPOCHS * per_epoch
     alpha = STARTING_ALPHA
 
     for epoch in range(EPOCHS):
+        print("%d | vecs: %s" % (epoch, vecs))
         for b in range(per_epoch):
             # take (1000x1000)
             randixs = np.random.randint(start, end, BATCHSIZE)
@@ -130,9 +132,12 @@ def perthread(tid):
             f = vecs[fixs]
             c = vecs[fixs + len(vocab)]
             
-            y = torch.matmul(f, torch.transpose(c, 0, 1))
+            # y = contains all dot products of each fixs with other fixs
+            y = torch.sigmoid(torch.matmul(f, torch.transpose(c, 0, 1)))
 
-            if b % (per_epoch // 10) == 0:
+            print("y(", y.shape, ") | y: ", y)
+
+            if b % max((per_epoch // 10), 1) == 0:
                 print("%d | epoch: %d | percent: %d%%" % (tid, epoch, (b / per_epoch) * 100))
 
             # get the words and fill in the dot products
@@ -141,11 +146,20 @@ def perthread(tid):
             # loss = \sum_ij (labels_ij - y_ij)^2 
             loss = labels.float() - y
             loss = loss * loss
+
+            # backprop loss
             loss = loss.sum(0).sum(0)
             loss.backward()
 
-            vecs.data.sub_(vecs.grad.data * alpha)
+            print("loss: ", loss)
+            print("vec before grad ", vecs)
+            print("grad: ", vecs.grad / (BATCHSIZE * BATCHSIZE))
+
+            # gradient descent
+            vecs.data.sub_(vecs.grad.data * alpha / (BATCHSIZE * BATCHSIZE))
             vecs.grad.data.zero_()
+            print("vec after grad ", vecs)
+
 
 
         print("%d | done with epoch: %d" % (tid, epoch))
@@ -167,18 +181,18 @@ def test():
     with torch.no_grad():
         while True:
             word = session.prompt(">")
-            if word not in syn0:
+            if word not in vocab:
                 print("%s out of corpus." % word)
                 continue
             dots = []
-            v = normalize(syn0[word])
+            v = normalize(vecs[vocabIxs[word]])
             print("v: %s" % v)
-            for (word2, v2) in syn0.items():
-                v2 = normalize(v2) 
+            for word2 in vocab:
+                v2 = normalize(vecs[vocabIxs[word2]]) 
                 dots.append((word2, v.dot(v2)))
             dots.sort(key=lambda wordvec: wordvec[1])
-            for (word, vec) in dots[:10]:
-                print("%s %20.4f" % (word.rjust(20, " "), vec))
+            for (word, d) in dots[::-1][:10]:
+                print("%s %20.4f" % (word.rjust(20, " "), d))
 
 def loadmodel():
     with open("model.out", "rb") as f:

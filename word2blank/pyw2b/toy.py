e3d1cc0583f1a0a4dea39307d6a24789abedaa27
Siddharth Bhat <siddu.druid@gmail.com>
2019-08-11 03:04:43 +0530

    have a version of word2vec that trains on GPU

--- word2blank/pyw2b/toy.py
+++ word2blank/pyw2b/toy.py
@@ -18,28 +18,57 @@ import tabulate
 from random import sample
 import pickle
 from prompt_toolkit import PromptSession
+import threading
 
 CORPUSPATH="text1"
-BATCHSIZE = 100
+NTHREADS = 1
+BATCHSIZE = 1000
 WINDOWSIZE = 4
 NNEGSAMPLES = 15
 EPOCHS = 15
-EMBEDSIZE = 2
+EMBEDSIZE = 100
 STARTING_ALPHA = 0.01
-DEVICE = torch.device(torch.cuda.device_count() - 1) if torch.cuda.is_available() else torch.device('cpu')
+# DEVICE = torch.device(torch.cuda.device_count() - 1) if torch.cuda.is_available() else torch.device('cpu')
+DEVICE = torch.device("cpu")
 
 with open(CORPUSPATH, "r") as f:
     corpus = np.array(f.read().split())
+print("corpus length: ", len(corpus))
 
+print("building vocab...")
 vocab = set(corpus)
+print(  "vocab length: ", len(vocab))
 
-print("creating syn0...")
-syn0 = { v : torch.randn(EMBEDSIZE, device=DEVICE, requires_grad=True, dtype=torch.float)
-                for v in vocab }
+# match each word in the vocabulary to an index
+print("creating vocabIxs")
+vocabIxs = dict(zip(vocab, range(len(vocab))))
 
-print("creating syn1neg...")
-syn1neg = { v : torch.randn(EMBEDSIZE, device=DEVICE, requires_grad=True, dtype=torch.float)
-                for v in vocab }
+print("creating neighbours tensor")
+# vocabNeighbours = torch.zeros([len(vocab), len(vocab)], dtype=torch.bool)
+vocabNeighbours = np.zeros([len(vocab), len(vocab)], dtype=np.bool)
+
+print("created ixedcorpus")
+ixedcorpus = np.zeros(len(corpus), dtype=np.int)
+
+print("ixing corpus")
+for i in range(len(corpus)):
+    ixedcorpus[i] = vocabIxs[corpus[i]]
+
+print("ixed corpus: ", ixedcorpus)
+part = len(corpus) // 10
+for i in range(len(corpus)):
+    iix = ixedcorpus[i]
+    if i % part == 0: print("*")
+    left = max(i-WINDOWSIZE, 0)
+    right = min(i+WINDOWSIZE, len(vocab) - 1)
+    vocabNeighbours[iix][np.arange(left, right)] = True
+
+print("vocab neighbours: ", vocabNeighbours)
+vocabNeighbours = torch.tensor(vocabNeighbours, device=DEVICE, dtype=torch.bool)
+print("sent vocab neighbours to device: ", vocabNeighbours)
+
+print("creating vecs")
+vecs = torch.randn([len(vocab) *2, EMBEDSIZE], device=DEVICE, requires_grad=True, dtype=torch.float)
 
 print("corpus length: %s" % len(corpus))
 print("vocab %s" % len(vocab))
@@ -54,23 +83,62 @@ def embedtensor(embeddict, ws):
 
 # fis, cis = 1 row, BATCH columns
 def trainsample(fis, cis, label, alpha):
-    # vec_fis, vec_cis = BATCH rows, EMBED coluns
-    vec_fis = [syn0[corpus[i]] for i in fis]
-    vec_cis = [syn1neg[corpus[i]] for i in cis]
+    i = 0
+    while i < BATCHSIZE:
+        i += 1
+        f = vecs[wordixs[corpus[i]]]
+        c = vecs[len(vocab) + wordixs[corpus[i]]]
+        d = f.dot(c)
 
-    dots = torch.stack([f.dot(c) for (f,c) in zip(vec_fis, vec_cis)])
-    loss =  (label - dots)
-    # loss^2
-    loss = loss.dot(loss)
-    loss.backward()
+        loss = (label - d) * (label - d)
+        loss.backward()
 
-    # need to index using data
-    for v in vec_fis + vec_cis:
-        v.data.sub_(v.grad.data * alpha)
-        # zero out the loss
-        v.grad.data.zero_()
+        # f.data.sub_(f.grad.data * alpha)
+        # f.grad.data.zero_()
+        # 
+        # c.data.sub_(c.grad.data * alpha)
+        # c.grad.data.zero_()
+    vecs.data.sub_(vecs.grad.data * alpha)
+    vecs.grad.data.zero_()
+    return 0
 
-    return float(loss)
+def trainthread(ix):
+    size = len(corpus) // NTHREADS
+    begin = min(size * ix, len(corpus) - 1)
+    end = min(size *(ix+1) - 1, len(corpus) - 1)
+
+    total = (EPOCHS * size) // BATCHSIZE
+    count = 0
+
+    last_seen_words = 0
+    total_loss = 0
+
+
+    alpha = STARTING_ALPHA
+
+    label1 = torch.ones(1, device=DEVICE)
+    label0 = torch.zeros(1, device=DEVICE)
+
+        # for fis in np.array_split(np.arange(begin,end), size // BATCHSIZE):
+        #     # positive sample training
+        #     # context words are focus words, with random perturbation
+        #     cis = fis + np.random.randint(-WINDOWSIZE,+WINDOWSIZE, size=fis.size, dtype=np.int32)
+        #     # make sure this is within bounds.
+        #     cis = np.clip(cis, 0, len(corpus) - 1)
+        #     total_loss += trainsample(fis, cis, label1, alpha)
+
+        #     # # random vector
+        #     # cis = np.random.randint(len(corpus), size=fis.size)
+        #     # total_loss += trainsample(fis, cis, label0, alpha)
+
+        #     count += 1
+        #     last_seen_words += 1
+
+        #     if (last_seen_words >= 1e1):
+        #         sys.stdout.write("\r%d: %f%% Loss: %4.2f\n" % (ix, (float(count) / total) * 100, total_loss))
+        #         sys.stdout.flush()
+        #         last_seen_words = 0
+        #         total_loss = 0
 
 
 def trainmodel():
@@ -79,33 +147,46 @@ def trainmodel():
     last_seen_words = 0
     total_loss = 0
     total = EPOCHS * len(corpus) // BATCHSIZE
+    per_epoch = len(corpus) // BATCHSIZE
     alpha = STARTING_ALPHA
-    for _ in range(EPOCHS):
-        # fi = focus index
-        for fis in np.array_split(np.arange(len(corpus), dtype=np.int32), len(corpus) // BATCHSIZE):
-            # positive sample training
-            # context words are focus words, with random perturbation
-            cis = fis + np.random.randint(-WINDOWSIZE,+WINDOWSIZE, size=fis.size,
-                                          dtype=np.int32)
-            # make sure this is within bounds.
-            cis = np.clip(cis, 0, len(corpus) - 1)
-            total_loss += trainsample(fis, cis, torch.tensor([1.0], device=DEVICE), alpha)
+    print("per_epoch: ", per_epoch)
 
-            # random vector
-            cis = np.random.randint(len(corpus), size=fis.size)
-            total_loss += trainsample(fis, cis, torch.tensor([0.0], device=DEVICE), alpha)
+    for epoch  in range(EPOCHS):
+        for b in range(per_epoch):
+            # take (1000x1000)
+            randixs = list(range(BATCHSIZE))
+            # get the words corresponding to the corpus
+            fixs = ixedcorpus[randixs]
 
-            count += 1
-            last_seen_words += 1
+            f = vecs[fixs]
+            c = vecs[fixs + len(vocab)]
+            
+            y = torch.matmul(f, torch.transpose(c, 0, 1))
 
-            if (last_seen_words > 1e3):
-                sys.stdout.write("\r%f%% Loss: %4.2f\n" % ((float(count) / total) * 100, total_loss))
-                sys.stdout.flush()
-                last_seen_words = 0
-                total_loss = 0
+            # get the words and fill in the dot products
+            print("filling in dot products in label...")
+            labels = vocabNeighbours[fixs].transpose(0, 1)[fixs].transpose(0, 1)
+            print("  filled.")
+
+            # loss = \sum_ij (labels_ij - y_ij)^2 
+            loss = labels.float() - y
+            loss = loss * loss
+            print("summing over pointwise loss...")
+            loss = loss.sum(0).sum(0)
+            print("loss:", loss)
+
+            print("backpropping loss...")
+            loss.backward()
+
+            print("updating vec by gradient")
+            vecs.data.sub_(vecs.grad.data * alpha)
+            vecs.grad.data.zero_()
+
+
+        print("    done with epoch: %d" % epoch)
 
     with open("model.out", "wb") as f:
-        pickle.dump(syn0, f)
+        pickle.dump(vecs, f)
 
 # assumes syn0 is initialized
 def test():

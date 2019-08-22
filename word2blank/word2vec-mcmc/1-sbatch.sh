e5ea814e1c2b6145948b484c4c169fbc3adf4446
Siddharth Bhat <siddu.druid@gmail.com>
2019-08-23 02:25:31 +0530

    add MCMC implementation. Unfortunately performs quite poorly, probably because it does not iterate on the entire state.

---
+++ word2blank/word2vec-mcmc/1-sbatch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --job-name=w2v
+#SBATCH --mail-type=END
+#SBATCH -o ./slurm/%j
+
+### SET NAME (NO .bin) ###
+NAME=TEXT1
+########
+########
+
+
+DATE=`date '+%Y-%m-%d--%H:%M:%S'`
+GITNAME=$(git rev-parse --short HEAD)
+FOLDERNAME=$GITNAME
+mkdir -p models/
+mkdir -p slurm/
+
+make word2vec
+./word2vec -alpha 0.02 -train text0 -cbow 0 -output models/text0 -size 10 \
+-window 4 -negative 4 -hs 0 -sample 1e-4 -threads 1 -binary 1 -iter 15
+# time ./word2vec -train text1 -output models/$GITNAME.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 40 -binary 1 -iter 15 
+# $(cd models; ln -s $GITNAME.bin $NAME.bin; cd ../)
+# ./1-eval.sh models/$NAME.bin
+# ./1-save-models.sh

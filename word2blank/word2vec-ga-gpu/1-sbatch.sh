3a1b43bacb81fbd027149af84c6096f3c19a52fe
Siddharth Bhat <siddu.druid@gmail.com>
2019-09-11 18:42:47 +0530

    commit working GPU code

---
+++ word2blank/word2vec-ga-gpu/1-sbatch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --job-name=w2v
+#SBATCH --mail-type=END
+#SBATCH -o ./slurm/%j
+
+### SET NAME (NO .bin) ###
+NAME=size8-window8-negative25-iter20
+########
+########
+
+
+DATE=`date '+%Y-%m-%d--%H:%M:%S'`
+GITNAME=$(git rev-parse --short HEAD)
+FOLDERNAME=$GITNAME
+mkdir -p models/
+mkdir -p slurm/
+
+make word2vec
+head -c 1000000 text8 > text0
+cuda-memcheck ./word2vec -train text0 -output models/text8-size=128.bin -cbow 0 -size 16  \
+    -window 4 -negative 10 -hs 0 -sample 1e-4 -threads 1 -binary 1 -iter 15 \
+    -alpha 0.01
+# ./1-save-models.sh
+# $(cd models; ln -s $GITNAME.bin $NAME.bin; cd ../)
+# ./1-eval.sh models/$NAME.bin
+# ./1-save-models.sh

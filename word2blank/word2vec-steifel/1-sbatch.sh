f9a8f1673d1c0c02e8738d915da1c138b85a6c0a
Siddharth Bhat <siddu.druid@gmail.com>
2019-11-12 09:20:27 +0530

    start writing code for word2vec on steifel

---
+++ word2blank/word2vec-steifel/1-sbatch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --time=200:00:00
+#SBATCH --nodes=1
+#SBATCH --job-name=w2v
+#SBATCH --mail-type=END
+#SBATCH -o ./slurm/%j
+#SBATCH -A nlp
+
+
+mkdir -p models/
+mkdir -p slurm/
+
+make word2vec distance
+head -c 1000000 text8 > text0
+time ./word2vec -train text8 -output models/text8 -cbow 0 -size 64 \
+    -window 4 -negative 0 -hs 1 -sample 1e-4 -threads 40 -binary 1 -iter 15 \
+    -alpha 0.03
+# ./1-save-models.sh
+# $(cd models; ln -s $GITNAME.bin $NAME.bin; cd ../)
+# ./1-eval.sh models/$NAME.bin
+# ./1-save-models.sh

cca392a90b629c941dd92ed6bab7f977952f2ea0
Siddharth Bhat <siddu.druid@gmail.com>
2019-11-18 17:56:54 +0530

    add training code for text8

--- word2blank/word2sn-with-reimann/1-sbatch.sh
+++ word2blank/word2sn-with-reimann/1-sbatch.sh
@@ -22,10 +22,10 @@ mkdir -p slurm/
 rm word2vecgpu
 make word2vecgpu
 # ./word2vec -train text8 -cbow 0 -output models/text8-size=200-window=8-negative=25  -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 40 -binary 1 -iter 15 
-./word2vecgpu -train text0 -cbow 0 \
-    -output models/xxxx \
+./word2vecgpu -train text8 -cbow 0 \
+    -output models/text8-size=1024-window=100-iter=25-negative=1 \
     -size 1024 -window 100 \
-    -negative 1 -hs 0 -sample 1e-4 -threads 1 -binary 1 -iter 35 -alpha 0.01
+    -negative 1 -hs 0 -sample 1e-4 -threads 1 -binary 1 -iter 25 -alpha 0.02
 
 # time ./word2vec -train text1 -output models/$GITNAME.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 40 -binary 1 -iter 15 
 # $(cd models; ln -s $GITNAME.bin $NAME.bin; cd ../)

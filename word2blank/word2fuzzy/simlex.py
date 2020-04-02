24ca963b89c1ec9ecade5ce2b2f947d79e4c0658
Alok Debnath <alok.debnath@research.iiit.ac.in>
2020-04-02 22:48:00 +0530

    Now shows KL-Divergence and Cross Entropy separately

--- word2blank/word2fuzzy/simlex.py
+++ word2blank/word2fuzzy/simlex.py
@@ -4,10 +4,11 @@ import sys
 import scipy.stats
 import numpy as np
 
-if (len(sys.argv) != 3): raise RuntimeError("usage: %s <word2vec-embed-path> <simlex-text-file-path", sys.argv[0])
+if (len(sys.argv) != 4): 
+    raise RuntimeError("usage: \n%s <word2vec-embed-path> <simlex-text-file-path> <1 or 2> \n1: KL-Divergence\n2: Cross Entropy"%(sys.argv[0]))
 
 TEMPFILEPATH="dump-accuracy-simlex.temp.txt"
-subprocess.call(["./dump-accuracy-simlex", sys.argv[1], sys.argv[2], TEMPFILEPATH])
+subprocess.call(["./dump-accuracy-simlex", sys.argv[1], sys.argv[2], sys.argv[3], TEMPFILEPATH])
 
 with open(TEMPFILEPATH, "r") as f:
     pairs = np.asarray([ [float(w) for w in l.strip().split()] for l in f.readlines()])

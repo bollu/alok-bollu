5e5236f3809b4067f64f5477f785d36b85074147
Siddharth Bhat <siddu.druid@gmail.com>
2020-04-04 00:01:26 +0530

    start writing composition code

---
+++ word2blank/word2vec/compute-composition.cpp
@@ -0,0 +1,78 @@
+//  Copyright 2013 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+
+#define max_size  2000         // max length of strings
+#define max_w  50              // max length of vocabulary entries
+#define max_n  80000L              // max # of top-N vectors
+
+char st1[max_size];
+char st2[max_size], st3[max_size], st4[max_size], bestw[max_n][max_size], file_name[max_size];
+float dist, len, bestd[max_n], vec[max_size];
+long long words, size, a, b, c, d, b1, b2, b3;
+float *M;
+char *vocab;
+
+const int ARGVECFILE = 1;
+const int ARGCOMPFILE = 2;
+int main(int argc, char **argv) {
+  FILE *f;
+  int TCN, CCN = 0, TACN = 0, CACN = 0, SECN = 0, SYCN = 0, SEAC = 0, SYAC = 0, QID = 0, TQ = 0, TQS = 0;
+  if (argc < 3) {
+    printf("Usage: ./compute-accuracy <FILE> <N>\n"
+            "- FILE contains word projections\n"
+            "- <N> is top-N vectors to use"); 
+    return 0;
+  }
+  strcpy(file_name, argv[1]);
+  f = fopen(file_name, "rb");
+  if (f == NULL) {
+    printf("Input file not found\n");
+    return -1;
+  }
+  fscanf(f, "%lld", &words); 
+  const long N = words; assert(N < max_n);
+  fscanf(f, "%lld", &size);
+  vocab = (char *)malloc(words * max_w * sizeof(char));
+  M = (float *)malloc(words * size * sizeof(float));
+  if (M == NULL) {
+    printf("Cannot allocate memory: %lld MB\n", words * size * sizeof(float) / 1048576);
+    return -1;
+  }
+  for (b = 0; b < words; b++) {
+    a = 0;
+    while (1) {
+      vocab[b * max_w + a] = fgetc(f);
+      if (feof(f) || (vocab[b * max_w + a] == ' ')) break;
+      if ((a < max_w) && (vocab[b * max_w + a] != '\n')) a++;
+    }
+    vocab[b * max_w + a] = 0;
+    for (a = 0; a < max_w; a++) vocab[b * max_w + a] = toupper(vocab[b * max_w + a]);
+    for (a = 0; a < size; a++) fread(&M[a + b * size], sizeof(float), 1, f);
+    len = 0;
+    for (a = 0; a < size; a++) len += M[a + b * size] * M[a + b * size];
+    len = sqrt(len);
+    for (a = 0; a < size; a++) M[a + b * size] /= len;
+  }
+  fclose(f);
+
+  return 0;
+}

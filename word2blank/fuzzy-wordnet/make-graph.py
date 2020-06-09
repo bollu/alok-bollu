1c2fef28b8869410d172eb28c3b50ce5534448e5
Alok Debnath <alok.debnath@research.iiit.ac.in>
2020-06-09 15:52:03 +0530

    Graph constructed. Need to run connected components test and other basic graph algorithms

--- word2blank/fuzzy-wordnet/make-graph.py
+++ word2blank/fuzzy-wordnet/make-graph.py
@@ -1,32 +1,39 @@
-import preprocess
-
+import preprocess as ps
 import os
 from tqdm import tqdm
 import numpy as np
 
 class Graph:
-    def __init__(self, emb):
+    def __init__(self, emb, nsim):
         self.emb = emb
-        self.VOCAB = len(emb)
+        self.V = len(emb)
+        self.nsim = nsim
         self.NDIMS = len(list(emb.values())[0])
-        return
+        self.graph = dict()
 
-    def construct_graph(self, nsim):
-        graph = np.full(-1, (self.NDIMS, self.NDIMS), dtype=int)
-        for w in self.emb:
-            w_ix = list(self.emb.keys())index(w)
-            sim_list = preprocess.topn_similarity(emb, w, nsim)
-            for sim in sim_list:
-                (word, wt) = sim
-                sim_ix = self.emb.index[word]
-                graph[w_ix][sim_ix] = wt
-        return graph
+    def addEdge(self, word):
+        self.graph[word] = ps.topn_similarity(self.emb, word, self.nsim)
+        return self.graph[word]
+
+    def makeGraph(self):
+        for w1 in tqdm(list(self.emb.keys())):
+            self.graph[w1] = self.addEdge(w1)
+        return self.graph
 
 class GraphOps:
     def __init__(self, graph):
-        sel.graph = graph
-
-    def 
+        self.graph = graph
 
 if __name__ == '__main__':
-
+    dirname = '../MODELS/'
+    fname = 'wiki-news-300d-1M.bin'
+    NDIMS = 300
+    VOCAB = 50000
+    nsim = 50
+    word_vecs = ps.load_embedding(os.path.join(dirname, fname), VOCAB)
+    word_vecs = ps.normalize(word_vecs, 0, NDIMS)
+    word_vecs = ps.discretize(word_vecs, 0, NDIMS)
+    g = Graph(word_vecs, nsim)
+    graph = g.makeGraph()
+    for key, val in graph.items():
+        print(key + '\t' + str(val))

cb2a3f910c621342ff70ddfe985391e4b3942df3
Alok Debnath <alok.debnath@research.iiit.ac.in>
2020-06-11 10:54:47 +0530

    Working on connected components code, need to try simple DFS first

--- word2blank/fuzzy-wordnet/make-graph.py
+++ word2blank/fuzzy-wordnet/make-graph.py
@@ -20,20 +20,67 @@ class Graph:
             self.graph[w1] = self.addEdge(w1)
         return self.graph
 
-class GraphOps:
+    def removeWts(self):
+        wtlessGraph = dict()
+        for node, sim in self.graph.items():
+            wtless = [s[0] for s in sim]
+            wtlessGraph[node] = wtless
+        return wtlessGraph
+
+class SCC:
     def __init__(self, graph):
         self.graph = graph
+        self.V = len(graph)
+
+    def DFS(self, v, visited):
+        visited[v] = True
+        print(v, end=' ')
+        for i in graph[self.graph.index(v)]:
+            if not visited[self.graph.index(i)]:
+                self.DFS(i, visited)
+   
+    def fillOrder(self, v, visited, stack):
+        visited[v] = True
+        for i in graph[self.graph.index(v)]:
+            if not visited[self.graph.index(i)]:
+                self.fillOrder(i, visited, stack)
+        stack.append(v)
+        return stack
+
+    def getTranspose(self):
+        g = Graph(self.V)
+        for i in self.graph:
+            for j in graph[i]:
+                if j not in g:
+                    g.addEdge(j)
+        return g
+
+    def getSCC(self):
+        stack = list()
+        visited = [False]*(self.V)
+        for i in range(self.V):
+            if not visited[i]:
+                stack = self.fillOrder(i, visited, stack)
+        g = self.getTranspose(self.graph)
+        visited = [False]*(self.V)
+        while stack:
+            i = stack.pop()
+            if visited[i] == False:
+                g.DFS(i, visited)
+                print('')
+
 
 if __name__ == '__main__':
     dirname = '../MODELS/'
     fname = 'wiki-news-300d-1M.bin'
     NDIMS = 300
-    VOCAB = 50000
-    nsim = 50
+    VOCAB = 500
+    nsim = 10
     word_vecs = ps.load_embedding(os.path.join(dirname, fname), VOCAB)
     word_vecs = ps.normalize(word_vecs, 0, NDIMS)
     word_vecs = ps.discretize(word_vecs, 0, NDIMS)
     g = Graph(word_vecs, nsim)
     graph = g.makeGraph()
-    for key, val in graph.items():
-        print(key + '\t' + str(val))
+    g2 = g.removeWts()
+    scc = SCC(g2)
+    scc.getSCC()

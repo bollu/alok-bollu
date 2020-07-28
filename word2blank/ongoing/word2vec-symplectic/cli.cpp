162e4aa51838451118a1c35a913671b635e461c4
Siddharth Bhat <siddu.druid@gmail.com>
2020-07-28 14:54:51 +0530

    nuke most of old CLI to build new CLI for symp

--- word2blank/ongoing/word2vec-symplectic/cli.cpp
+++ word2blank/ongoing/word2vec-symplectic/cli.cpp
@@ -42,10 +42,6 @@ Vec *M;
 char *vocab;
 long long words, size;
 
-float mk01(float r) {
-    return powf(2, r);
-}
-
 void normalizeVec(Vec v) {
     float totalsize = 0;
     for(int i = 0; i < size; ++i) totalsize += v[i];
@@ -181,30 +177,8 @@ std::tuple<AST, char *> parse_(char *str) {
 
 AST parse(char *str) { return std::get<0>(parse_(str)); }
 
-float entropylog(float x) {
-    if (x < 1e-4) {
-        return 0;
-    }
-    return log(x);
-}
-
-float entropy(Vec v) {
-
-    float totalsize = 0;
-    for(int i = 0; i < size; ++i) totalsize += v[i];
-    for(int i = 0; i < size; ++i) v[i] /= totalsize;
-
-    float H = 0;
-    for(int i = 0; i < size; ++i) 
-        H += -v[i] * entropylog(v[i]) - (1 - v[i]) * entropylog(1 - v[i]);
-    return H;
-}
-
-
 
 Vec interpret(AST ast) {
-
-    const float STRONG_THRESHOLD = 0.5 / size;
     std::cout << "interpreting: ";
     ast.print();
     std::cout << std::endl;
@@ -215,18 +189,17 @@ Vec interpret(AST ast) {
             if (it == word2vec.end()) {
                 cout << "unable to find word: |" << ast.s() << "|\n";
                 goto INTERPRET_ERROR;
-            } else {
-                cout << ast.s() << " : ";
-                Vec out = new float[size];
-                for(int i = 0; i < size; ++i) out[i] = it->second[i];
-
-                for(int i = 0; i < std::min<int>(3, size); i++) {
-                    cout << setprecision(1) << out[i] << " ";
-                }
-                cout << "\n";
-                return out;
             }
-            assert(false && "unreachable");
+            cout << ast.s() << " : ";
+            Vec out = new float[size];
+            for(int i = 0; i < size; ++i) out[i] = it->second[i];
+
+            for(int i = 0; i < std::min<int>(3, size); i++) {
+                cout << setprecision(1) << out[i] << " ";
+            }
+            cout << "\n";
+            return out;
+            goto INTERPRET_ERROR;
         }
 
         case ASTTy::List: {
@@ -239,175 +212,8 @@ Vec interpret(AST ast) {
           }
 
           const std::string command = ast.at(0).s();
-
-          if (command == "and") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int i = 2; i < ast.size(); ++i) {
-                  Vec w = interpret(ast.at(i));
-                  if (!w) goto INTERPRET_ERROR;
-
-                  for(int j = 0; j < size; ++j) {
-                      out[j] *= w[j];
-                  }
-              }
-
-              return out;
-          } 
-          else if (command == "or") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int i = 2; i < ast.size(); ++i) {
-                  Vec w = interpret(ast.at(i));
-                  if (!w) goto INTERPRET_ERROR;
-
-                  for(int j = 0; j < size; ++j) {
-                      out[j] = out[j] +  w[j] - out[j] * w[j];
-                  }
-              }
-
-              return out;
-          } 
-          else if (command == "min") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int i = 2; i < ast.size(); ++i) {
-                  Vec w = interpret(ast.at(i));
-                  if (!w) goto INTERPRET_ERROR;
-
-                  for(int j = 0; j < size; ++j) {
-                      out[j] = min(out[j], w[j]);
-                  }
-              }
-
-              return out;
-          }
-          else if (command == "max") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int i = 2; i < ast.size(); ++i) {
-                  Vec w = interpret(ast.at(i));
-                  if (!w) goto INTERPRET_ERROR;
-
-                  for(int j = 0; j < size; ++j) {
-                      out[j] = max(out[j], w[j]);
-                  }
-              }
-
-              return out;
-          }
-          // https://en.wikipedia.org/wiki/Fuzzy_set#Fuzzy_set_operations
-          else if (command == "difference" || command == "diff") {
-              if (ast.size() != 3) {
-                  cout << "usage: difference <w1> <w2>\n";
-                  goto INTERPRET_ERROR;
-              }
-
-              Vec l = interpret(ast.at(1));
-              Vec r = interpret(ast.at(2));
-
-              Vec out = new float[size];
-              for(int i = 0; i < size; ++i) {
-                  out[i] = l[i] - min(l[i], r[i]);
-              }
-              normalizeVec(out);
-              return out;
-          }
-
-          else if (command == "analogy") {
-              if (ast.size() != 4) {
-                  cout << "usage: analogy <w1> <w2> <w3?\n";
-                  goto INTERPRET_ERROR;
-              }
-
-              // a : b :: x : ?
-              Vec a = interpret(ast.at(1));
-              Vec b = interpret(ast.at(2));
-              Vec x = interpret(ast.at(3));
-              normalizeVec(a);
-              normalizeVec(b);
-              normalizeVec(x);
-
-              Vec out = new float[size];
-              for(int i = 0; i < size; ++i) {
-                  // a : b :: x : ?
-                  // (A U X) / B
-                  float delta = b[i] + x[i] - min(b[i] + x[i], a[i]);
-                  out[i] = delta;
-              }
-              normalizeVec(out);
-              return out;
-          }
-
-         // https://en.wikipedia.org/wiki/%C5%81ukasiewicz_logic
-          else if (command == "strongmax") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int i = 2; i < ast.size(); ++i) {
-                  Vec w = interpret(ast.at(i));
-                  if (!w) goto INTERPRET_ERROR;
-
-                  normalizeVec(out);
-                  normalizeVec(w);
-
-                  for(int j = 0; j < size; ++j) {
-                      out[j] = max<float>(0.0, out[j] + w[j] - 1.0 / size);
-                  }
-              }
-
-              return out;
-          }
-         // https://en.wikipedia.org/wiki/%C5%81ukasiewicz_logic
-          else if (command == "strongmin") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int i = 2; i < ast.size(); ++i) {
-                  Vec w = interpret(ast.at(i));
-                  if (!w) goto INTERPRET_ERROR;
-
-                  normalizeVec(out);
-                  normalizeVec(w);
-
-                  for(int j = 0; j < size; ++j) {
-                      out[j] = min<float>(0, out[j] + w[j]);
-                  }
-              }
-
-              return out;
-          }
-          else if (command == "not") {
-              Vec out = interpret(ast.at(1));
-              if (!out) goto INTERPRET_ERROR;
-
-              for(int j = 0; j < size; ++j) {
-                  out[j] = 1 - out[j];
-              }
-
-              return out;
-
-
-          } else if (command == "entropy") {
-              float H = 0;
-              Vec out = interpret(ast.at(1));
-              H += entropy(out);
-
-              cout << "entropy: " << setprecision(5) <<  H << "\n";
-              return nullptr;
-
-          } else {
-              cout << "unknown command: " << command;
-              cout << "\n\t"; ast.print();
-              goto INTERPRET_ERROR;
-          }
-
-          assert(false && "unreachable");
-        }
+          goto INTERPRET_ERROR;
+      }
 
         case ASTTy::Null:
         default:
@@ -419,32 +225,6 @@ INTERPRET_ERROR:
     return nullptr;
 }
 
-// completions for linenoise
-void completion(const char *buf, linenoiseCompletions *lc) {
-    int ix = strlen(buf) - 1;
-    for(; ix >= 0; ix--) {
-        if (buf[ix] == ' ' || buf[ix] == '(' || buf[ix] == ')') {
-            break;
-        }
-    }
-
-    ix++;
-    if (ix == strlen(buf)) { return; }
-
-    for (int i = words - 1; i >= 0; i--)  {
-        char *w = vocab + i * max_w;
-        if (strstr(w, buf + ix) == w) {
-            // take buf till ix
-            std::string completion(buf, ix);
-            completion += std::string(w);
-
-            char *ccompletion = new char[completion.size()+2];
-            strcpy(ccompletion, completion.c_str());
-
-            linenoiseAddCompletion(lc, ccompletion);
-        }
-    }
-}
 
 // plot dimension usage
 void dimension_usage() {
@@ -509,89 +289,6 @@ void printCloseWords(Vec vec) {
     printf("\n");
 }
 
-void printAscByEntropy() {
-    printf("Words sorted by entropy (lowest):\n");
-    float vals[words];
-    float bestd[words];
-    char bestw[N][max_size];
-
-    float minentropy = -1;
-    for (int a = 0; a < N; a++) bestd[a] = 100;
-    for (int a = 0; a < N; a++) bestw[a][0] = 0;
-    for (int c = 0; c < words; c++) {
-      const float dist = entropy(M[c]);
-
-      for (int a = 0; a < N; a++) {
-        if (dist < bestd[a]) {
-          for (int d = N - 1; d > a; d--) {
-            bestd[d] = bestd[d - 1];
-            strcpy(bestw[d], bestw[d - 1]);
-          }
-          bestd[a] = dist;
-          strcpy(bestw[a], &vocab[c * max_w]);
-          break;
-        }
-      }
-    }
-    for (int a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
-    plotHistogram("entropies", vals, words, 10);
-    printf("\n");
-}
-
-
-void printDescByEntropy() {
-    printf("Words sorted by entropy (highest):\n");
-    float vals[words];
-    float bestd[words];
-    char bestw[N][max_size];
-
-    float minentropy = -1;
-    for (int a = 0; a < N; a++) bestd[a] = -100;
-    for (int a = 0; a < N; a++) bestw[a][0] = 0;
-    for (int c = 0; c < words; c++) {
-      const float dist = entropy(M[c]);
-
-      for (int a = 0; a < N; a++) {
-        if (dist > bestd[a]) {
-          for (int d = N - 1; d > a; d--) {
-            bestd[d] = bestd[d - 1];
-            strcpy(bestw[d], bestw[d - 1]);
-          }
-          bestd[a] = dist;
-          strcpy(bestw[a], &vocab[c * max_w]);
-          break;
-        }
-      }
-    }
-    for (int a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
-    plotHistogram("entropies", vals, words, 10);
-    printf("\n");
-}
-
-void printDistributionOfProbs() {
-    static const int NPROBS = 100;
-    int probs[NPROBS];
-    for(int i = 0; i < NPROBS; ++i) probs[i] = 0;
-
-    // focus on the probability range from 0 to 0.2
-    // 0 -> 0
-    // 0.2 -> NPROBS
-    for(int i = 0; i < words; ++i) {
-        for(int j = 0; j < size; ++j) {
-            const float f = M[i][j];
-            probs[int(floor(f * (NPROBS+1) / 0.2))]++;
-        }
-    }
-
-    int total = 0;
-    for(int i = 0; i < NPROBS; ++i) total += probs[i];
-
-    cout << "probability distribution across all vectors:\n";
-    for(int i = 0; i < NPROBS; ++i) {
-        cout << setprecision(2) << float(i) * 0.2 / (NPROBS) << " : " << probs[i]  << "\n";
-    }
-    cout << "----\n";
-}
 
 int main(int argc, char **argv) {
     char file_name[512];
@@ -634,29 +331,15 @@ int main(int argc, char **argv) {
         for(int i = 0; i < size; ++i) {
             float fl;
             fread(&M[b][i], sizeof(float), 1, f);
-            M[b][i] = mk01(M[b][i]);
-            setsize += M[b][i];
         }
         
-        for(int i = 0; i < size; ++i) {
-            M[b][i] /= setsize;
-        }
-
         word2vec[std::string(vocab+b*max_w)] = M[b];
-
-
     }
 
 
     fclose(f);
 
-    printAscByEntropy();
-    printDescByEntropy();
-    printDistributionOfProbs();
-
-
     linenoiseHistorySetMaxLen(10000);
-    linenoiseSetCompletionCallback(completion);
     while (1) {
         char *s = linenoise(">");
         AST ast = parse(s);
@@ -667,9 +350,6 @@ int main(int argc, char **argv) {
 
         if (ast.ty() == ASTTy::Null) continue;
 
-        const Vec v = interpret(ast);
-        if (!v) continue;
-
-        printCloseWords(v);
+        interpret(ast);
     }
 }

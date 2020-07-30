54c7cab0b5b7d0a9f9316ba8f42ecaa6a1f11932
Alok Debnath <alok.debnath@research.iiit.ac.in>
2020-07-31 02:58:07 +0530

    modified preprocess to use gensim for fasttext

--- word2blank/ongoing/fuzzy-wordnet/preprocess.py
+++ word2blank/ongoing/fuzzy-wordnet/preprocess.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import numpy as np
 from sklearn import preprocessing
 from gensim.models.keyedvectors import KeyedVectors
+from gensim.models.keyedvectors import Word2VecKeyedVectors
 from scipy import spatial
 from collections import OrderedDict
 #from numba import jit, cuda
@@ -13,16 +14,13 @@ def load_embedding(fpath, VOCAB):
     """
         Using Gensim to load FastText embeddings
     """
+    print("Loading embeddings...")
     emb = dict()
     wv_from_bin = KeyedVectors.load_word2vec_format(fpath, limit=VOCAB)
-    for word, vector in zip(wv_from_bin.vocab, wv_from_bin.vectors):
+    for word, vector in tqdm(zip(wv_from_bin.vocab, wv_from_bin.vectors)):
         coefs = np.asarray(vector, dtype='float32')
-        if not re.match(r'\w+', word):
-            continue
-        elif word.lower() not in emb:
-            emb[word.lower()] = coefs
-        else:
-            emb[word.lower()] = np.mean([emb[word.lower()], coefs], axis=0)
+        if word not in emb:
+            emb[word] = coefs
     return emb
 
 def normalize(word_vecs, axis, NDIMS):
@@ -30,6 +28,7 @@ def normalize(word_vecs, axis, NDIMS):
         axis(0): sum up the columns
         axis(1): sum up the rows
     """
+    print("Normalizing across axis = %d" % axis)
     vec_mat = np.array(list(word_vecs.values()))
     vec_mat = np.exp(vec_mat)                       # e^x for x in all vectors
     vec_mat = preprocessing.normalize(vec_mat, norm='l1', axis=axis)
@@ -45,6 +44,7 @@ def discretize(word_vecs, axis, NDIMS):
             x = 0 otherwise
         threshold = average
     """
+    print("Discretizing across = %d" % axis)
     vec_mat = np.array(list(word_vecs.values()))
     threshold = np.mean(vec_mat, axis=axis)
     vec_mat = (vec_mat >= threshold) * 1
@@ -53,6 +53,24 @@ def discretize(word_vecs, axis, NDIMS):
     # word_vecs['<TOP>'] = np.ones(NDIMS, dtype=int)
     # word_vecs['<BOT>'] = np.zeros(NDIMS, dtype=int)
     return word_vecs
+
+def gensim_store(fname, embs, VOCAB, NDIMS):
+    """
+        Ref :https://github.com/RaRe-Technologies/gensim/blob/3d2227d58b10d0493006a3d7e63b98d64e991e60/gensim/models/keyedvectors.py#L130
+        Write out the discretized word representations into a file, so that we can load more of them
+        `fname` : the file used to save the vectors in
+        `embs`: word embeddings in dictionary format
+        `VOCAB` : number of words
+        `NDIMS` : number of dimensions
+    """
+    print("Storing vectors")
+    g = open(fname, 'wb+')
+    g.write(gensim.utils.to_utf8('%s %s\n' % (VOCAB, NDIMS)))
+    for word, vector in tqdm(embs.items()):
+        g.write(gensim.utils.to_utf8('%s %s\n' % (word, ' '.join('%f' % val for val in vector))))
+    g.close()
+    return
+
 
 def decode(word_vecs, vec):
     """
@@ -121,38 +139,40 @@ def logicalxor(emb, w1, w2):
     return np.logical_xor(emb[w1], emb[w2])
 
 if __name__ == '__main__':
-    dirname = '../MODELS/'
+    dirname = '../../utilities/MODELS/'
     fname = 'wiki-news-300d-1M.bin'
-    NDIMS = 300
-    VOCAB = 50000
+    # NDIMS = 300
+    VOCAB = 999994
     nsim = 10
-    word_vecs = load_embedding(os.path.join(dirname, fname), VOCAB)
-    word_vecs = normalize(word_vecs, 0, NDIMS)
-    word_vecs = discretize(word_vecs, 0, NDIMS)
-
+    # word_vecs = load_embedding(os.path.join(dirname, fname), VOCAB)
+    # word_vecs = normalize(word_vecs, 0, NDIMS)
+    # word_vecs = discretize(word_vecs, 0, NDIMS)
+    # gensim_store('test.bin', word_vecs, VOCAB, NDIMS)
     w1 = 'condition'
     w2 = 'relation'
-
+    disc_vecs = KeyedVectors.load_word2vec_format("test.bin", binary=False) 
+    word_vecs = load_embedding("test.bin", VOCAB) 
+    
     print('sim({})'.format(w1), end='\t')
-    print(topn_similarity(word_vecs, w1, nsim))
-
+    print(disc_vecs.similar_by_word(w1, topn=nsim, restrict_vocab=False))
+    
     print('sim({})'.format(w2), end='\t')
-    print(topn_similarity(word_vecs, w2, nsim))
+    print(disc_vecs.similar_by_word(w2, topn=nsim, restrict_vocab=False))
     
     print('{} u {}'.format(w1, w2), end='\t')
-    print(decode(word_vecs, union(word_vecs, w1, w2)))
-    
+    print(disc_vecs.similar_by_vector(union(word_vecs, w1, w2), topn=1))
+
     print('{} n {}'.format(w1, w2), end='\t')
-    print(decode(word_vecs, intersection(word_vecs, w1, w2)))
+    print(disc_vecs.similar_by_vector(intersection(word_vecs, w1, w2), topn=1))
 
     print('{} - {}'.format(w1, w2), end='\t')
-    print(decode(word_vecs, difference(word_vecs, w1, w2)))
+    print(disc_vecs.similar_by_vector(difference(word_vecs, w1, w2), topn=1))
 
     print('{} OR {}'.format(w1, w2), end='\t')
-    print(decode(word_vecs, logicalor(word_vecs, w1, w2)))
+    print(disc_vecs.similar_by_vector(logicaland(word_vecs, w1, w2), topn=1))
     
     print('{} AND {}'.format(w1, w2), end='\t')
-    print(decode(word_vecs, logicaland(word_vecs, w1, w2)))
+    print(disc_vecs.similar_by_vector(logicalor(word_vecs, w1, w2), topn=1))
 
     print('{} XOR {}'.format(w1, w2), end='\t')
-    print(decode(word_vecs, logicalxor(word_vecs, w1, w2)))
+    print(disc_vecs.similar_by_vector(logicalxor(word_vecs, w1, w2), topn=1))

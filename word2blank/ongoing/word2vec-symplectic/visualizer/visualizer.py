91112c54157bacf5d4d5e8d800e62afb0748a203
Alok Debnath <alok.debnath@research.iiit.ac.in>
2020-08-03 20:55:40 +0530

    removed asymptotic computation

--- word2blank/ongoing/word2vec-symplectic/visualizer/visualizer.py
+++ word2blank/ongoing/word2vec-symplectic/visualizer/visualizer.py
@@ -6,22 +6,17 @@ from tqdm import tqdm
 from mpl_toolkits.mplot3d import Axes3D
 import sys
 
-def load_embedding(fpath, x):
+def load_embedding(fpath, x, wordlist):
     print("Loading embeddings...")
     emb = dict()
-    wv_from_bin = KeyedVectors.load_word2vec_format(fpath, binary=x, limit=1000000)
-    for word, vector in tqdm(zip(wv_from_bin.vocab, wv_from_bin.vectors)):
-        coefs = np.asarray(vector, dtype='float32')
-        if word not in emb:
-            emb[word] = coefs
-    return emb
+    wv_from_bin = KeyedVectors.load_word2vec_format(fpath, binary=x)
+    vs = np.array([wv_from_bin.get_vector(w) for w in wordlist])
+    # for word, vector in tqdm(zip(wv_from_bin.vocab, wv_from_bin.vectors)):
+    #    coefs = np.asarray(vector, dtype='float32')
+    #    if word not in emb:
+    #        emb[word] = coefs
+    return vs
 
-def get_embs(emb, wordlist):
-    mat = list()
-    for word in wordlist:
-        mat.append(emb[word])
-    mat = np.array(mat)
-    return mat
 
 def plot(mat, fname):
     X, Y, Z, U, V, W = zip(*mat)
@@ -37,10 +32,10 @@ def plot(mat, fname):
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("usage: %s <path to embeddings file>" % (sys.argv[1], ))
-    emb1 = load_embedding(sys.argv[1], True)
+    # emb1 = load_embedding(sys.argv[1], True)
     # emb2 = load_embedding('posvel_models/syn1neg-symp-size4-dim6-text8.bin', True)
     wordlist = ['good', 'better', 'best', 'bad', 'worse', 'worst']
-    mat1 = get_embs(emb1, wordlist)
+    mat1 = load_embedding(sys.argv[1], True, wordlist)
     # mat2 = get_embs(emb2, wordlist)
     print(mat1)
     # print(mat2)

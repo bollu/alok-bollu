23a5f7c9323d8fd038e4eeca80b82151b5462963
Alok Debnath <alok.debnath@research.iiit.ac.in>
2020-08-05 22:57:31 +0530

    PCA completed

--- word2blank/ongoing/word2vec-symplectic/visualizer/visualizer.py
+++ word2blank/ongoing/word2vec-symplectic/visualizer/visualizer.py
@@ -6,36 +6,50 @@ import gensim
 from tqdm import tqdm
 from mpl_toolkits.mplot3d import Axes3D
 import sys
+from sklearn import decomposition
 
-def load_embedding(fpath, x, wordlist):
+def load_embedding(fpath, x, wordlist, dim):
     print("Loading embeddings...")
     emb = dict()
     wv_from_bin = KeyedVectors.load_word2vec_format(fpath, binary=x)
-    embs = np.array([wv_from_bin.get_vector(w) for w in wordlist])
+    embs = np.array([wv_from_bin.get_vector(w) for w in wv_from_bin.vocab])
     vpos, vmom  = np.hsplit(embs, 2)
-    vpos = np.corrcoef(vpos)
-    vmom = np.corrcoef(vmom)
-    posval, posvec = np.linalg.eig(vpos)
-    momval, momvec = np.linalg.eig(vmom)
-    ixs = np.argsort(-posval)
-    posval = posvec[ixs]
-    posvec = posvec[:, ixs]
-    pca_posvec = posvec[:, :3]
-    ixs = np.argsort(-momval)
-    momval = momvec[ixs]
-    momvec = momvec[:, ixs]
-    pca_momvec = momvec[:, :3]
+    pca = decomposition.PCA(n_components=dim)
+    pca.fit(vpos)
+    pca_posvec = pca.transform(vpos)
+    pca.fit(vmom)
+    pca_momvec = pca.transform(vmom)
     vs = np.concatenate((pca_posvec, pca_momvec), axis=1)
-    return vs
+    wvs = []
+    for word in wordlist:
+        ix = wv_from_bin.vocab[word].index
+        wvs.append(vs[ix])
+    wvs = np.array(wvs)
+    return wvs
 
 
-def plot(mat, fname):
-    X, Y, Z, U, V, W = zip(*mat)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.quiver(X, Y, Z, U, V, W)
-    ax.set_xlim([-3, 3])
-    ax.set_ylim([-3, 3])
+def plot(mat, fname, dim):
+    X = 0
+    Y = 0
+    Z = 0
+    U = 0
+    V = 0
+    W = 0
+    fig = 0
+    ax = 0
+    if dim == 3:
+        X, Y, Z, U, V, W = zip(*mat)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.quiver(X, Y, Z, U, V, W)
+        ax.set_zlim([-1, 1])
+    elif dim == 2:
+        X, Y, U, V = zip(*mat)
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.quiver(X, Y, U, V, units='xy', angles='uv')
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
     plt.draw()
     plt.show()
     plt.savefig(fname)
@@ -43,12 +57,8 @@ def plot(mat, fname):
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("usage: %s <path to embeddings file>" % (sys.argv[1], ))
-    # emb1 = load_embedding(sys.argv[1], True)
-    # emb2 = load_embedding('posvel_models/syn1neg-symp-size4-dim6-text8.bin', True)
-    wordlist = ['good', 'better', 'best', 'bad', 'worse', 'worst']
-    mat1 = load_embedding(sys.argv[1], True, wordlist)
-    # mat2 = get_embs(emb2, wordlist)
+    wordlist = ['france', 'paris', 'germany', 'berlin']
+    dim = 2
+    mat1 = load_embedding(sys.argv[1], True, wordlist, dim)
     print(mat1)
-    # print(mat2)
-    plot(mat1, 'rays.png')
-    # plot(mat2, 'syn1neg-rays.png')
+    plot(mat1, 'rays.png', dim)

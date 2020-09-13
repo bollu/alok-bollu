5200f540c2f0f119fbc495f52d3033a4c3ca0601
Siddharth Bhat <siddu.druid@gmail.com>
2020-09-13 16:11:38 +0530

    add sbatch file for text8

---
+++ word2blank/ongoing/word2grass/1-sbatch-text8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --job-name=w2gr
+#SBATCH --mail-type=END
+#SBATCH -o ./slurm/%j
+
+set -e
+set -o xtrace
+
+mkdir -p models/
+mkdir -p models/standard
+mkdir -p models/iters
+mkdir -p models/windowsize
+mkdir -p slurm/
+
+rm word2grass || true
+make word2grass
+
+time ./word2grass -train ../../utilities/text8 -output models/grass-text8.bin \
+    -alpha 0.1 -cbow 0 -n 19 -p 5 -window 8 \
+    -hs 1 -sample 1e-4 -threads 30 -binary 1 -iter 5

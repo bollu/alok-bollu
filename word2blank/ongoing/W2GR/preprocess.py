afed343e09268ed296c89461887d059b94007d7b
SouvikBan <banerjeesouvik123@gmail.com>
2021-06-05 22:08:16 +0530

    preprocess file changed

--- word2blank/ongoing/W2GR/preprocess.py
+++ word2blank/ongoing/W2GR/preprocess.py
@@ -1,62 +1,63 @@
 import os
 import re
 
-def preprocess(orig_file):
+def preprocess(orig_file, typefile):
     print(orig_file)
     labels = []
     reviews = []
-    for filename in os.listdir(orig_file+'/neg'):
+    for filename in os.listdir(orig_file+typefile+'/neg'):
         info = filename.split(".")[0]
         label = info.split("_")[1]
         labels.append(label)
-        with open(orig_file+'/neg/'+filename,"r") as fin:
+        with open(orig_file+typefile+'/neg/'+filename,"r") as fin:
             content = fin.read()
             #lower case
             content = content.lower()
             #removing html tags
             content = re.sub('<.*?>',' ',content)
-            #removing special characters (punctuation) '@,!' e.t.c.
-            #content = re.sub('\W',' ', content) 
-            #removing single characters
-            content = re.sub('\s+[a-zA-Z]\s+',' ', content)
+            #add space between punctuations and word, numbers
+            content = re.sub(r"([\w/'+$\s-]+|[^\w/'+$\s-]+)\s*", r"\1 ", content)
+            content = re.sub(r"([\d/'+$\s]+|[^\d/'+$\s]+)\s*", r"\1 ", content)
+            content = re.sub(r"(\w+)'s", r"\1 's", content)
             #substituting multiple spaces with single space
             content = re.sub('\s+',' ', content)
             reviews.append(content)
-    for filename in os.listdir(orig_file+'/pos'):
+    for filename in os.listdir(orig_file+typefile+'/pos'):
         info = filename.split(".")[0]
         label = info.split("_")[1]
         labels.append(label)
-        with open(orig_file+'/pos/'+filename,"r") as fin:
+        with open(orig_file+typefile+'/pos/'+filename,"r") as fin:
             content = fin.read()
             #lower case
             content = content.lower()
             #removing html tags
             content = re.sub('<.*?>',' ',content)
-            #removing special characters (punctuation) '@,!' e.t.c.
-            #content = re.sub('\W',' ', content) 
-            #removing single characters
-            content = re.sub('\s+[a-zA-Z]\s+',' ', content)
+            #add space between punctuations and word, numbers
+            content = re.sub(r"([\w/'+$\s-]+|[^\w/'+$\s-]+)\s*", r"\1 ", content)
+            content = re.sub(r"([\d/'+$\s]+|[^\d/'+$\s]+)\s*", r"\1 ", content)
+            content = re.sub(r"(\w+)'s", r"\1 's", content)
             #substituting multiple spaces with single space
             content = re.sub('\s+',' ', content)
             reviews.append(content)
-    # for filename in os.listdir(orig_file+'/unsup'):
-    #     with open(orig_file+'/unsup/'+filename,"r") as fin:
-    #         content = fin.read()
-    #         #lower case
-    #         content = content.lower()
-    #         #removing html tags
-    #         content = re.sub('<.*?>',' ',content)
-    #         #removing special characters (punctuation) '@,!' e.t.c.
-    #         #content = re.sub('\W',' ', content) 
-    #         #removing single characters
-    #         content = re.sub('\s+[a-zA-Z]\s+',' ', content)
-    #         #substituting multiple spaces with single space
-    #         content = re.sub('\s+',' ', content)
-    #         reviews.append(content)
-    with open("test.txt","w+") as fout:
+    if typefile == "train":
+        for filename in os.listdir(orig_file+typefile+'/unsup'):
+            with open(orig_file+typefile+'/unsup/'+filename,"r") as fin:
+                content = fin.read()
+                #lower case
+                content = content.lower()
+                #removing html tags
+                content = re.sub('<.*?>',' ',content)
+                #add space between punctuations and word, numbers
+                content = re.sub(r"([\w/'+$\s-]+|[^\w/'+$\s-]+)\s*", r"\1 ", content)
+                content = re.sub(r"([\d/'+$\s]+|[^\d/'+$\s]+)\s*", r"\1 ", content)
+                content = re.sub(r"(\w+)'s", r"\1 's", content)
+                #substituting multiple spaces with single space
+                content = re.sub('\s+',' ', content)
+                reviews.append(content)
+    with open(typefile+".txt","w+") as fout:
         for review in reviews:
             fout.write(review+"\n")
-    with open("test_lbl.txt","w+") as fout:
+    with open(typefile+"_lbl.txt","w+") as fout:
         for label in labels:
             fout.write(label+"\n")
 
@@ -65,9 +66,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='sentiment-analysis',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--dataset', default='aclImdb')
+    parser.add_argument('--dataset', default='dataset')
     #parser.add_argument('--emb_file', default='jose.txt')
-    #sparser.add_argument('--domain', default='headlines')
+    #parser.add_argument('--domain', default='headlines')
 
     args = parser.parse_args()
-    preprocess(orig_file=os.path.join("./datasets", args.dataset))
+    preprocess(args.dataset, "train")
+    preprocess(args.dataset, "test")
